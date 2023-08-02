1e3707f72cdadfc4b76583a14099ec922c27c3d0
Weston Nelson <dogooddowell@protonmail.com>
2023-08-01 23:12:24 -0500

    Update parameters.ts
    Signed-off-by: Weston Nelson <dogooddowell@protonmail.com>

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -1,7 +1,7 @@
 export const contractConst = "0x8dB6b24c4718FA4E9b3d1bF166e29247174E8f8f";
-
 export const chainConst = "base";
-
 export const primaryColorConst = "blue";
-
 export const themeConst = "dark";
+export const relayerUrlConst = ""; 
+export const biconomyApiKeyConst = "";
+export const biconomyApiIdConst = "";
