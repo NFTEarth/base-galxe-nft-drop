7b2bfbd5491d381f1837595e60650a86ebb0dc19
Weston Nelson <dogooddowell@protonmail.com>
2023-08-01 23:03:45 -0500

    Update parameters.ts
    Signed-off-by: Weston Nelson <dogooddowell@protonmail.com>

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -1,21 +1,7 @@
-/** Change these values to configure the application for your own use. **/
+export const contractConst = "0x8dB6b24c4718FA4E9b3d1bF166e29247174E8f8f";
 
-// Your smart contract address (available on the thirdweb dashboard)
-// For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
-export const contractConst = "0x436492DBc2E30E56FaC8F2297BD1964833c0687d";
+export const chainConst = "base";
 
-// The name of the chain your contract is deployed to.
-// Provide a string for standard chains, or import one from @thirdweb-dev/chains package.
-// Learn more: https://portal.thirdweb.com/react/react.thirdwebprovider#activechain-recommended
-export const chainConst = "mumbai";
-
-// Configure the primary color for buttons and other UI elements
 export const primaryColorConst = "blue";
 
-// Choose between "light" and "dark" mode
 export const themeConst = "dark";
-
-// Gasless relayer configuration options
-export const relayerUrlConst = ""; // OpenZeppelin relayer URL
-export const biconomyApiKeyConst = ""; // Biconomy API key
-export const biconomyApiIdConst = ""; // Biconomy API ID
